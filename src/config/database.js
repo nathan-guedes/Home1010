b6e07665468496f55b5f6172f772d6cf9997d839
Nathan Guedes <nathan.guedes99@gmail.com>
2020-02-06 00:44:30 -0300

    Ambiente Criado

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    port: '5435',
+    database: 'home1010',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
